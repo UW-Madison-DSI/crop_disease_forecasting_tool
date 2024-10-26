f1b7c8f0e8e03b01ad40539c5542e12005195fab
Maria Oros <maria.oros1201@gmail.com>
2024-10-26 13:17:25 -0500

    ADD/CUS: minor, increase the zoom

--- app/functions/logic.R
+++ app/functions/logic.R
@@ -6,28 +6,10 @@ library(zoo)
 library(ggplot2)
 library(dplyr)  # Load dplyr for the pipe operator
 library(httr)
+library(scales)
 
 
-################################################################ Dates
-# Function to convert Fahrenheit to Celsius
-fahrenheit_to_celsius <- function(temp_f) {
-  (temp_f - 32) * 5/9
-}
-
-# Function to convert current time to GMT and subtract a number of months
-from_ct_to_gmt <- function(current_time, mo){
-  # Subtract months from the current time in Central Time
-  past_time_ct <- current_time - months(mo)
-  
-  # Convert both dates to Unix timestamps in GMT
-  start_time <- as.integer(as.POSIXct(past_time_ct, tz = "GMT"))
-  end_time <- as.integer(as.POSIXct(current_time, tz = "GMT"))
-  
-  return(list(
-    start_time_gmt = start_time,
-    end_time_gmt = end_time
-  ))
-}
+source("functions/auxiliar_functions.R")
 
 ################################################################ Preparation
 
@@ -35,7 +17,7 @@ base_url <- 'https://wisconet.wisc.edu'
 url_ts <- "https://connect.doit.wisc.edu/forecasting_crop_disease"
 
 
-################################################################ Function to get weather data from the API
+################################################################ Function to get weather data from Wisconet API
 api_call_wisconet_data_daily <- function(station, start_time, end_time) {
   endpoint <- paste0('/api/v1/stations/', station, '/measures')
   
@@ -112,18 +94,7 @@ api_call_wisconet_data_daily <- function(station, start_time, end_time) {
   }
 }
 
-################################################################ Function to plot the data
-api_call_wisconet_plot <- function(df) {
-  ggplot(df, aes(x = collection_time)) +
-    geom_line(aes(y = air_temp_avg_c, color = "Daily Average")) +
-    geom_line(aes(y = air_temp_avg_c_30d_ma, color = "30-day Moving Average")) +
-    labs(title = "Average Temperature (Celsius)",
-         x = "Date",
-         y = "Temperature (°C)",
-         color = "Legend") +
-    theme_minimal() +
-    theme(legend.position = "bottom")
-}
+
 
 # Function to fetch and plot data
 fetch_at <- function(station, start_time, end_time) {
@@ -234,70 +205,10 @@ fetch_rh_above_90_daily <- function(station, start_time, end_time) {
 }
 
 
-# Define a function to create the weather plot
-plot_weather_data <- function(data, station) {
-  # Create the plot
-  weather_plot <- data %>%
-    ggplot(aes(x = collection_time)) +
-    
-    # Min temperature and its 30-day moving average
-    geom_line(aes(y = air_temp_min_c, color = "Min Temp (°C)")) +
-    geom_line(aes(y = air_temp_min_c_30d_ma, color = "Min Temp (°C) (30d MA)"), linetype = "dashed") +
-    
-    # Avg temperature and its 30-day moving average
-    geom_line(aes(y = air_temp_avg_c, color = "Avg Temp (°C)")) +
-    geom_line(aes(y = air_temp_avg_c_30d_ma, color = "Avg Temp (°C) (30d MA)"), linetype = "dashed") +
-    
-    # Max temperature and its 30-day moving average
-    geom_line(aes(y = air_temp_max_c, color = "Max Temp (°C)")) +
-    geom_line(aes(y = air_temp_max_c_30d_ma, color = "Max Temp (°C) (30d MA)"), linetype = "dashed") +
-    
-    # Add RH to the plot using the secondary y-axis
-    #geom_line(aes(y = rh_max, color = "Max RH (%)"), linetype = "solid") +
-    
-    # Primary y-axis for temperature
-    #scale_y_continuous(
-    #  name = "Temperature (C)",  # Label for the primary y-axis
-    #  sec.axis = sec_axis(~ ., name = "Relative Humidity (%)")  # Secondary y-axis for RH
-    #) +
-    
-    # Title, labels, and theme
-    labs(title = paste("Air Temperature (°C) for", station),
-         x = "Date",y='Air Temperature (°C)') +
-    
-    # Minimal theme
-    theme_minimal() +
-    
-    # Move the legend below the plot
-    theme(
-      legend.position = "bottom",         # Position the legend below the plot
-      legend.direction = "horizontal",    # Arrange the legend items horizontally
-      legend.title = element_blank(),     # Remove the legend title
-      legend.text = element_text(size = 10)  # Customize legend text size
-    ) +
-    
-    # Color manual assignment
-    scale_color_manual(values = c(
-      "Min Temp (°C)" = "#5DA5DA",            # Soft blue
-      "Min Temp (°C) (30d MA)" = "#ADD8E6",   # Light blue
-      "Avg Temp (°C)" = "#60BD68",            # Soft green
-      "Avg Temp (°C) (30d MA)" = "#B2E2B2",   # Light green
-      "Max Temp (°C)" = "#FAA43A",            # Light orange
-      "Max Temp (°C) (30d MA)" = "#FDDC9B",   # Light peach
-      "Max RH (%)" = "#B276B2",              # Soft purple
-      "RH (30d MA)" = "#CFCFCF",             # Light gray for subtler contrast
-      "Dew Point (°C)" = "#FFC107"            # Muted yellow
-    ))
-  
-  
-  # Return the plot
-  return(weather_plot)
-}
-
-################################################################ Call Tarspot API
 ################################################################################
 ################################################################################
-## Experimenting using the logic of the model
+################################################################ Call Tarspot 
+## Using the logic of the model
 logistic <- function(logit) {
   exp(logit) / (1 + exp(logit))
 }
@@ -354,6 +265,8 @@ get_risk_probability <- function(station_id, station_name,
   }
 }
 
+
+################### Still comparing, probably will be deprecated one
 get_risk_probability_by_api <- function(station_id, station_name, 
                                  risk_threshold,
                                  mat_30dma, max_rh_30dma,
@@ -402,23 +315,6 @@ get_risk_probability_by_api <- function(station_id, station_name,
       Risk_Class = NA
     ))
   }
-}
-
-
-library(scales)
-
-plot_trend <- function(df, station){
-  ggplot(df, aes(x = Date, y = Risk)) +
-    geom_line(color = "#0C7BDC") +
-    geom_point(color = "#FFC20A") +
-    geom_text(aes(label = Risk_Class),
-              vjust = -0.5,
-              color = "black") +
-    labs(title = paste(station$name, "Station,", station$region, "Region,", station$state),
-         x = "Date",
-         y = "Probability of Tarspot (%)") +
-    scale_y_continuous(labels = percent_format(scale = 1)) +  # Formats y-axis as percentages
-    theme_minimal()
 }
 
 
