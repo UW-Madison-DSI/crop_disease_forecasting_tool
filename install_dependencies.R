83d7edbc3eb368dc6f8cbf0c09ea90b7ae0d2941
Maria Oros <maria.oros1201@gmail.com>
2024-11-04 16:57:57 -0600

    Update install_dependencies.R

--- install_dependencies.R
+++ install_dependencies.R
@@ -1,8 +1,8 @@
-# install_dependencies.R
+required_packages <- c("testthat", "dplyr", "httr","lubridate",
+                       "jsonlite","zoo","ggplot2","scales",
+                       'shiny','leaflet','shinydashboard','shinyWidgets','flexdashboard',
+                       'gridExtra')  
 
-required_packages <- c("testthat", "dplyr", "httr")  # Add only the packages you need
-
-# Install packages if not already installed
 for (pkg in required_packages) {
   if (!requireNamespace(pkg, quietly = TRUE)) {
     install.packages(pkg)
